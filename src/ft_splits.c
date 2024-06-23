2a10d3e0de5eb45266105bf55dd74365cea37956
Pablo Escobar <sataniv.rider@gmail.com>
2024-06-23 22:15:48 +0200

    still working on ft_splits

--- src/ft_splits.c
+++ src/ft_splits.c
@@ -6,7 +6,7 @@
 /*   By: blackrider <blackrider@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 21:08:25 by blackrider        #+#    #+#             */
-/*   Updated: 2024/06/21 23:03:16 by blackrider       ###   ########.fr       */
+/*   Updated: 2024/06/23 22:12:40 by blackrider       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,195 +15,277 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void	printllist(void *data)
-{
-	printf("[0]: %d\t[1]: %d\n", ((int *)(data))[0], ((int *)(data))[1]);
-}
-
-void	*freet_splt(t_splt *data)
-{
-	if (!data)
-		return (NULL);
-	free(data->lens);
-	free(data);
-	return (NULL);
-}
-
-t_splt	*crtt_splt(const char *args, const char **splt)
-{
-	t_splt	*data;
-
-	data = malloc(sizeof(t_splt));
-	if (!data)
-		return (NULL);
-	data->lens = malloc(sizeof(int) * ft_matsize(splt));
-	if (!(data->lens))
-		freet_splt(data);
-	data->args = args;
-	data->splt = splt;
-	data->args_s = args;
-	data->args_f = args + ft_strlen(args) - 1;
-	data->sizesplt = 0;
-	while (splt[data->sizesplt])
-	{
-		data->lens[data->sizesplt] = ft_strlen(splt[data->sizesplt]);
-		++data->sizesplt;
-	}
-	return (data);
-}
-
-static int	*crtdata(int a, int b)
+static int		*crtdata(int x, int size)
 {
 	int	*tmp;
 
 	tmp = malloc(2 * sizeof(int));
-	tmp[0] = a;
-	tmp[1] = b;
+	*tmp = x;
+	tmp[1] = size;
 	return (tmp);
-}	
-
-static char		checkbn(t_splt *d, int i, int j)
-{
-	if (d->args + i == d->args_s)
-		return (0);
-	if (d->args + i - d->lens[j] >= d->args_s)
-		if (!ft_strncmp(d->args + i - d->lens[j], d->splt[j], d->lens[j]))
-			return (0);
-	if (d->args + d->lens[j] < d->args_f)
-		if (ft_strncmp(d->args + i + d->lens[j], d->splt[j], d->lens[j]))
-			return (1);
-	return (0);
 }
 
-static char		checksplt(t_splt *d, int i_args)
+char	*ft_strlcmp(const char *str, const char *cmp)
 {
-	int	j;
-
-	j = 0;
-	while (d->splt[j])
+	while (*str && (*str == *cmp))
 	{
-		if (ft_strncmp(d->args + i_args, d->splt[j], d->lens[j]))
-		{
-			++j;
-			continue ;
-		}
-		if (checkbn(d, i_args, j))	
-			return (d->lens[j]);
-		++j;
+		++str;
+		++cmp;
 	}
-	return (0);
+	if (!(*str) || !(*cmp))
+		return (str);
+	return (NULL);
 }
 
-t_llist	*llistadd_back(t_llist **llst, t_llist *newnode)
+static char		*issplt(const char *str, const char **splt)
 {
-	t_llist	*tmp;
+	char	*tmp;
 
-	if (!newnode)
-		return (NULL);
-	if (!(*llst))
+	while (*splt)
 	{
-		*llst = newnode;
-		return (newnode);
+		tmp = ft_strlcmp(str, *splt);
+		if (tmp)
+			return (tmp);
+		++splt;
 	}
-	tmp = *llst;
-	if (tmp->previous)
-	{
-		while (tmp->previous->next)
-			tmp = tmp->previous;
-		newnode->previous = tmp->previous;
-		tmp->previous->next = newnode;
-	}
-	else
-	{
-		newnode->previous = tmp;
-		tmp->next = newnode;
-	}
-	tmp->previous = newnode;
-	return (newnode);
+	return (str);
 }
 
-static t_llist	*setcrd(t_splt *d)
+static char		*checksplt(const char *str, const char **splt)
 {
-	int		j;
-	int		i;
+	char	*tmp;
+
+	tmp = issplt(str, splt);
+	while (*str && tmp != str)
+	{
+		str = tmp;
+		tmp = issplt(str, splt);
+	}
+	return (str);
+}
+
+static t_llist	*setstrs(const char *str, const char **splt)
+{
+	char	*tmp;
 	t_llist	*llst;
-	t_llist	*newnode;
+	t_llist	*node;
 
-	i = -1;
-	llst = NULL;
-	while (d->args[++i])
+	str = checksplt(str, splt);
+	llst = llistnewnode(crtdata(tmp, 0));
+	node = llst;
+	while (*str)
 	{
-		j = checksplt(d, i);
-		if (!j)
-			continue ;
-		if (!llst)
+		tmp = checksplt(str, splt);
+		if (tmp > str)
 		{
-			llst = llistnewnode(crtdata(0, i - 1));
-			newnode = llst;
-			continue ;
+			((int *)(node->data))[1] = tmp - str;
+			node = llistadd_back(llst, llistnewnode(crtdata(tmp, 0)));
+			str = tmp;
 		}
-		newnode = llistadd_back(&llst,
-			llistnewnode(crtdata(((int *)(newnode->data))[1] + 2, i - 1)));
+		else
+			++str;
 	}
-	// llistadd_back(&llst,
-	// 	llistnewnode(crtdata(((int *)(newnode->data))[1] + 2, i - 1)));
-	return (llst);
 }
 
-// char	**ft_splits(const char *str, const char **splt)
+// static t_llist	*setstrs(const char *str, const char **splt)
 // {
-// 	int		size;
-// 	char	**strs;
+// 	int		i;
+// 	char	*tmp;
 // 	t_llist	*llst;
 
-// 	llst = setcrd(str, splt);
-// 	size = llistsize(llst);
-// 	strs = malloc((size + 1) * sizeof(char *));
-// 	if (!strs)
-// 		return (llistclear(&llst, ft_freenode));
-// 	strs[size] = NULL;
-// 	llst = llst->previous;
-// 	while (size)
+// 	str = checksplt(str, splt);
+// 	llst = llistnewnode(crtdata(tmp - str, 0));
+// 	while (*str)
 // 	{
-// 		--size;
-// 		strs[size] = ft_strndup((str + ((int *)(llst->d))[0]),
-// 			((int *)(llst->d))[1]);
-// 		llst = llst->previous;
+// 		tmp = checksplt(str, splt);
+// 		if (tmp > str)
+// 		{
+// 			if (llst->previous)
+// 				((int *)(llst->previous->data))[1] = tmp - str;
+// 			else
+// 				((int *)(llst->data))[1] = tmp - str;
+// 			llistadd_back(llst, llistnewnode(crtdata(tmp - str, 0)));
+// 			str = tmp;
+// 		}
+// 		else
+// 			++str;
 // 	}
-// 	llistclear(&llst, ft_freenode);
-// 	return (strs);
+// 	return (llst);
 // }
 
-void	printmatrix(char **matrix)
-{
-	while (*matrix)
-	{
-		printf("|%s|\n", *matrix);
-		++matrix;
-	}
-}
 
 
-int	main()
-{
-	char	**splt;
-	t_splt	*data;
-	t_llist	*llst;
 
-	splt = ft_split("|", '0');
-	data = crtt_splt("|a|b|d|", (const char **)splt);
-	printf("ARGV:\t%s\n", data->args);
-	printmatrix(splt);
-	for (int i = 0; i < data->sizesplt; ++i)
-		printf("[%d]\t%d\n", i, data->lens[i]);
-	llst = setcrd(data);
-	llistiter(llst, printllist);
-	// char	**res;
 
-	// res = ft_splits("|||avf|||bg|$01234$0000  123   $$", " |$");
-	// printmatrix(res);
-	return (0);
-}
+
+
+
+
+
+
+
+
+// void	printllist(void *data)
+// {
+// 	printf("[0]: %d\t[1]: %d\n", ((int *)(data))[0], ((int *)(data))[1]);
+// }
+
+// void	*freet_splt(t_splt *data)
+// {
+// 	if (!data)
+// 		return (NULL);
+// 	free(data->lens);
+// 	free(data);
+// 	return (NULL);
+// }
+
+// t_splt	*crtt_splt(const char *args, const char **splt)
+// {
+// 	t_splt	*data;
+
+// 	data = malloc(sizeof(t_splt));
+// 	if (!data)
+// 		return (NULL);
+// 	data->lens = malloc(sizeof(int) * ft_matsize(splt));
+// 	if (!(data->lens))
+// 		freet_splt(data);
+// 	data->args = args;
+// 	data->splt = splt;
+// 	data->args_s = args;
+// 	data->args_f = args + ft_strlen(args) - 1;
+// 	data->sizesplt = 0;
+// 	while (splt[data->sizesplt])
+// 	{
+// 		data->lens[data->sizesplt] = ft_strlen(splt[data->sizesplt]);
+// 		++data->sizesplt;
+// 	}
+// 	return (data);
+// }
+
+// static int	*crtdata(int a, int b)
+// {
+// 	int	*tmp;
+
+// 	tmp = malloc(2 * sizeof(int));
+// 	tmp[0] = a;
+// 	tmp[1] = b;
+// 	return (tmp);
+// }	
+
+// static char		checkbn(t_splt *d, int i, int j)
+// {
+// 	if (d->args + i == d->args_s)
+// 		return (0);
+// 	if (d->args + i - d->lens[j] >= d->args_s)
+// 		if (!ft_strncmp(d->args + i - d->lens[j], d->splt[j], d->lens[j]))
+// 			return (0);
+// 	if (d->args + d->lens[j] < d->args_f)
+// 		if (ft_strncmp(d->args + i + d->lens[j], d->splt[j], d->lens[j]))
+// 			return (1);
+// 	return (0);
+// }
+
+// static char		checksplt(t_splt *d, int i_args)
+// {
+// 	int	j;
+
+// 	j = 0;
+// 	while (d->splt[j])
+// 	{
+// 		if (ft_strncmp(d->args + i_args, d->splt[j], d->lens[j]))
+// 		{
+// 			++j;
+// 			continue ;
+// 		}
+// 		if (checkbn(d, i_args, j))	
+// 			return (d->lens[j]);
+// 		++j;
+// 	}
+// 	return (0);
+// }
+
+// static t_llist	*setcrd(t_splt *d)
+// {
+// 	int		j;
+// 	int		i;
+// 	t_llist	*llst;
+// 	t_llist	*newnode;
+
+// 	i = -1;
+// 	llst = NULL;
+// 	while (d->args[++i])
+// 	{
+// 		j = checksplt(d, i);
+// 		if (!j)
+// 			continue ;
+// 		if (!llst)
+// 		{
+// 			llst = llistnewnode(crtdata(0, i - 1));
+// 			newnode = llst;
+// 			continue ;
+// 		}
+// 		newnode = llistadd_back(&llst,
+// 			llistnewnode(crtdata(((int *)(newnode->data))[1] + 2, i - 1)));
+// 	}
+// 	// llistadd_back(&llst,
+// 	// 	llistnewnode(crtdata(((int *)(newnode->data))[1] + 2, i - 1)));
+// 	return (llst);
+// }
+
+// // char	**ft_splits(const char *str, const char **splt)
+// // {
+// // 	int		size;
+// // 	char	**strs;
+// // 	t_llist	*llst;
+
+// // 	llst = setcrd(str, splt);
+// // 	size = llistsize(llst);
+// // 	strs = malloc((size + 1) * sizeof(char *));
+// // 	if (!strs)
+// // 		return (llistclear(&llst, ft_freenode));
+// // 	strs[size] = NULL;
+// // 	llst = llst->previous;
+// // 	while (size)
+// // 	{
+// // 		--size;
+// // 		strs[size] = ft_strndup((str + ((int *)(llst->d))[0]),
+// // 			((int *)(llst->d))[1]);
+// // 		llst = llst->previous;
+// // 	}
+// // 	llistclear(&llst, ft_freenode);
+// // 	return (strs);
+// // }
+
+// void	printmatrix(char **matrix)
+// {
+// 	while (*matrix)
+// 	{
+// 		printf("|%s|\n", *matrix);
+// 		++matrix;
+// 	}
+// }
+
+
+// int	main()
+// {
+// 	char	**splt;
+// 	t_splt	*data;
+// 	t_llist	*llst;
+
+// 	splt = ft_split("|", '0');
+// 	data = crtt_splt("|a|b|d|", (const char **)splt);
+// 	printf("ARGV:\t%s\n", data->args);
+// 	printmatrix(splt);
+// 	for (int i = 0; i < data->sizesplt; ++i)
+// 		printf("[%d]\t%d\n", i, data->lens[i]);
+// 	llst = setcrd(data);
+// 	llistiter(llst, printllist);
+// 	// char	**res;
+
+// 	// res = ft_splits("|||avf|||bg|$01234$0000  123   $$", " |$");
+// 	// printmatrix(res);
+// 	return (0);
+// }
 
 // char	*ft_strstr(const char *str, const char *srch, size_t len)
 // {
--- src/setindata.c
+++ src/setindata.c
@@ -6,7 +6,7 @@
 /*   By: blackrider <blackrider@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 13:20:54 by blackrider        #+#    #+#             */
-/*   Updated: 2024/06/21 15:41:37 by blackrider       ###   ########.fr       */
+/*   Updated: 2024/06/23 22:15:10 by blackrider       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,99 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-"   a   b   c   "
+static int		*crtdata(int x, int size)
+{
+	int	*tmp;
+
+	tmp = malloc(2 * sizeof(int));
+	*tmp = x;
+	tmp[1] = size;
+	return (tmp);
+}
+
+const char	*ft_strlcmp(const char *str, const char *cmp)
+{
+	while (*str && (*str == *cmp))
+	{
+		++str;
+		++cmp;
+	}
+	if (!(*str) || !(*cmp))
+		return (str);
+	return (NULL);
+}
+
+static int		issplt(const char *str, const char **splt)
+{
+	char	*tmp;
+
+	while (*splt)
+	{
+		tmp = ft_strlcmp(str, (const char *)*splt);
+		if (tmp)
+			return (tmp - str);
+		++splt;
+	}
+	return (0);
+}
+
+static int		checksplt(const char *str, const char **splt, int strsize)
+{
+    int tmp;
+    int i;
+
+    i = issplt(str, splt);
+    if (!i)
+        return (0);
+    while (i < strsize)
+    {
+        tmp = issplt(str + i, splt);
+        if (!tmp)
+            return (i);
+        i += tmp;
+    }
+    return (strsize);
+}
+
+static t_llist	*setstrs(const char *str, const char **splt)
+{
+    int     i;
+    int     offset;
+    int     strsize;
+    t_llist *llst;
+
+    strsize = ft_strlen(str);
+    i = checksplt(str, splt, strsize);
+    llst = llistnewnode(crtdata(i, 0));
+    while (i < strsize)
+    {
+        offset = checksplt(str + i, splt, strsize);
+        if (offset)
+        {
+            if (llst->previous)
+                ((int *)(llst->previous->data))[1] = i + offset;
+            else
+                ((int *)(llst->data))[1] = i + offset;
+            i += offset;
+            llistadd_back(&llst, llistnewnode(crtdata(i, 0)));
+        }
+        ++i;
+    }
+    return (llst);
+}
+
+void	printllist(void *data)
+{
+	printf("[0]: %d\t[1]: %d\n", ((int *)(data))[0], ((int *)(data))[1]);
+}
+
+int main()
+{
+    t_llist *llst;
+    char    **splt;
+
+    splt = ft_split("|||0||0|", '|');
+    llst = setstrs("gerp -A2 || wc -l | cd ..", (const char **)splt);
+    llistiter(llst, printllist);
+    return (0);    
+}
