c9496397315814d009e3ad83f4a3624f41b8216c
Pablo Escobar <sataniv.rider@gmail.com>
2024-07-26 17:47:22 +0200

    still on netdata

--- src/netdata/src/netdata.c
+++ src/netdata/src/netdata.c
@@ -6,7 +6,7 @@
 /*   By: Pablo Escobar <sataniv.rider@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 15:02:44 by blackrider        #+#    #+#             */
-/*   Updated: 2024/07/26 17:11:31 by Pablo Escob      ###   ########.fr       */
+/*   Updated: 2024/07/26 17:47:02 by Pablo Escob      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,47 +18,11 @@
 
 t_cchar	*cmpstrv(t_cchar *args, t_cchar **strv)
 {
+	if (!strv)
+		return (NULL);
 	while (*strv && !ft_strlcmp(args, *strv))
 		++strv;
 	return (*strv);
-}
-
-int		checkinitdtd(t_cchar *args, t_splqt *splt)
-{
-	int		tmp;
-
-	tmp = cmpqts(args, splt->rdr);
-	if (tmp)
-		return (tmp);
-	tmp = cmpqts(args, splt->splts);
-	if (tmp)
-	{
-		printf("%s\t\'%s\'", TOKENERROR, splt->splts[tmp]);
-		return (-1);
-	}
-	return (IOSIZE);
-}
-
-int	setinitdt(t_cchar *args, t_splqt *splt, t_llist *llargt, t_argv *argvt)
-{
-	int	oper;
-
-	oper = cmpqts(args + T_ARG(llargt)->x, splt);
-	if (oper < 0)
-			return (0);
-	switch (oper)
-	{
-	case (I_APPOFILE):
-		argvt->appnd = 1;
-		argvt->outfile = T_ARG(llargt)->arg;
-		break ;
-	case (I_OFILE):
-		argvt->outfile = T_ARG(llargt)->arg;
-		break ;
-	case (I_IFILE):
-		argvt->infile = T_ARG(llargt)->arg;
-	}
-	return (1);
 }
 
 int		checksplt(t_cchar *args, t_cchar **escptn, t_cchar **splts)
@@ -66,12 +30,34 @@ int		checksplt(t_cchar *args, t_cchar **escptn, t_cchar **splts)
 	int	oper;
 
 	if (!(*args))
-		return (-1);
+		return (0);
 	if (cmpstrv(args, escptn))
 		return (0);
 	oper = cmpqts(args, splts);
 	if (oper)
 		return (oper + O_HRDOC);
+	return (0);
+}
+
+int	setrdrdt(t_splqt *splt, t_llist **llargt, t_argv *argvt)
+{
+	if (!(argvt->oper))
+			return (0);
+	switch (argvt->oper)
+	{
+	case (O_APPND):
+		argvt->appnd = 1;
+		argvt->outfile = T_ARG(*llargt)->arg;
+		break ;
+	case (O_OFILE):
+		argvt->outfile = T_ARG(*llargt)->arg;
+		break ;
+	case (O_IFILE):
+		argvt->infile = T_ARG(*llargt)->arg;
+	default:
+		return (argvt->oper);
+	}
+	*llargt = (*llargt)->next;
 	return (0);
 }
 
@@ -96,23 +82,49 @@ int		setargv(t_cchar *args, t_splqt *splt, t_llist **llargt, t_argv *argvt)
 	int	size;
 	t_cchar	**tmp;
 
-	size = getsize(args, splt, llargt, argvt);
+	size = getsize(args, splt, *llargt, argvt);
 	argvt->argv = malloc((size + 1) * sizeof(t_cchar *));
 	if (!(argvt->argv))
-		return (ft_perror("MALLOC ERROR!!!: in setargv"));
+	{
+		ft_perror("MALLOC ERROR!!!: in setargv");
+		return (1);
+	}
 	argvt->argv[size] = NULL;
 	tmp = argvt->argv;
 	while (size)
 	{
-		*tmp = T_ARG(llargt)->arg;
-		llargt = llargt->next;
+		*tmp = T_ARG(*llargt)->arg;
+		*llargt = (*llargt)->next;
 		--size;
 		++tmp;
 	}
-	return (llargt);
+	return (0);
 }
 
+int		setendargvt(t_cchar *args, t_splqt *splt, t_llist **llargt, t_argv *argvt)
+{
+	if (argvt->oper != O_APPND && argvt->oper != O_IFILE
+		&& argvt->oper != O_OFILE)
+			return (0);
+	if (setrdrdt(splt, llargt, argvt))
+		return (0);
+	argvt->oper = checksplt(args + T_ARG(*llargt)->x, NULL, splt);
+	*llargt = (*llargt)->next;
+	return (0);
+}
 
+int	initdat(t_cchar *args, t_splqt *splt, t_llist **llargt, t_argv *argvt)
+{
+	if (!(*llargt)->previous || (*llargt)->previous->next)
+		return (0);
+	argvt->oper = checksplt(args + T_ARG(*llargt)->x, NULL, splt);
+	if (!setrdrdt(splt, llargt, argvt))
+	{
+		printf("%s\t\'%s\'", TOKENERROR, splt->splts[argvt->oper]);
+		return (1);
+	}
+	return (0);
+}
 
 t_argv	*setargvt(t_cchar *args, t_splqt *splt, t_llist **llargt)
 {
@@ -122,12 +134,11 @@ t_argv	*setargvt(t_cchar *args, t_splqt *splt, t_llist **llargt)
 	argvt = crtargvt();
 	if (!argvt)
 		return (ft_perror("MALLOC ERROR!!!: crtargvt"));
-	if ((*llargt)->previous && !(*llargt)->previous->next)
-		if (!setinitdt(args, splt, llargt, argvt))
-			return (llistclear(&argvt, freeargvt));
-	tmp = setargv(args, splt, llargt, argvt);
-	tmp = 
-	*llargt = tmp;
+	if (initdat(args, splt, llargt, argvt))
+		return (delargvt(argvt));
+	if (setargv(args, splt, llargt, argvt))
+		return (delargvt(argvt));
+	setendargvt(args, splt, llargt, argvt);
 	return (argvt);
 }
 
@@ -149,3 +160,18 @@ t_llist	*netdata(t_cchar *args, t_hash *hst)
 }
 
 
+// int		checkinitdtd(t_cchar *args, t_splqt *splt)
+// {
+// 	int		tmp;
+
+// 	tmp = cmpqts(args, splt->rdr);
+// 	if (tmp)
+// 		return (tmp);
+// 	tmp = cmpqts(args, splt->splts);
+// 	if (tmp)
+// 	{
+// 		printf("%s\t\'%s\'", TOKENERROR, splt->splts[tmp]);
+// 		return (-1);
+// 	}
+// 	return (IOSIZE);
+// }
